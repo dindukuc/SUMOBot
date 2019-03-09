53ffc4d882891d94a4d7e0be84b567f81371a7e2
Cody McCall <mccallco@flip3.engr.oregonstate.edu>
2019-03-09 15:04:39 -0800

    Edited keytest.py

--- code/sumotests/keytest.py
+++ code/sumotests/keytest.py
@@ -1,1 +1,30 @@
-import guizero
+from guizero import TextBox, Text
+
+def read_key(event_data):
+    print("Read : " + event_data.key)
+
+def forward():
+    print('F')
+
+def backwards():
+    print('B')
+
+def right():
+    print('R')
+
+def left():
+    print('L')
+
+app = App("Display Sensor", layout="auto")
+
+key_w = TextBox(app)
+
+key_w.when_key_pressed = read_key()
+
+
+text = Text(app, text ="", visible=True)
+
+#button4.after(3, clear)
+
+
+app.display()
