afef664499f363a5bda56bd829ab826a30e7631a
Chetan Dindukurthi <dindukuc@oregonstate.edu>
2019-03-12 13:04:00 -0700

    updated gui

--- code/sumotests/testgui.py
+++ code/sumotests/testgui.py
@@ -21,7 +21,7 @@ ser = serial.Serial('/dev/ttyS0',
 
 
 def getBtns():
-        Btns = [0, 0, 0, 0]
+    Btns = [0, 0, 0, 0]
     ser.write(b'H')
     data = int(ser.read(1).hex(), 16)
 
@@ -29,9 +29,9 @@ def getBtns():
     Btns[1] =   (data & 0b00000010) >> 1
     Btns[2] =   (data & 0b00000001) 
     Btns[3] =   (data & 0b00000100) >> 2
-    text.value = str(Btns[0]) + "/" + str(Btns[1]) + "/" + str(Btns[2]) + "/" + str(Btns[3])
+    bttn.value = "Button sensors: " + str(Btns[0]) + "/" + str(Btns[1]) + "/" + str(Btns[2]) + "/" + str(Btns[3])
 
-    return Btns
+    #return Btns
 
 def getIR():
     IR = [0, 0, 0, 0]
@@ -42,37 +42,48 @@ def getIR():
     IR[1] =   (data & 0b00000010) >> 1
     IR[2] =   (data & 0b00000001) 
     IR[3] =   (data & 0b00000100) >> 2
-    text.value = str(IR[0]) + "/" + str(IR[1]) + "/" + str(IR[2]) + "/" + str(IR[3])
+    ir.value = "IR sensors: " + str(IR[0]) + "/" + str(IR[1]) + "/" + str(IR[2]) + "/" + str(IR[3])
 
 
-    return IR
+    #return IR
 
 def getVolt():
     ser.write(b'E')
     data = ser.read(1).hex()
-    
+    batt.value = "Battery Voltage: " + str(data)
 
 def getMotor():
     ser.write(b'M')
     data = ser.read(2).hex()
-
-
+	mtr.value = "Motor Speed: " + str(data)
+	#ml = data[0] 
+	#mr = 
+	#speedL = int(data1,16)* (100/127)
+	#speedR = int(data2,16)* (100/127)
+	
+	   
     
+def updateTof():
+	tof.value = "ToF sensor: " + str(vl53.range)
     
-def updateSensor():
 
 
-    text1.value = "ToF sensor: " + str(vl53.range)
-    text2.value = "Battery Voltage: ")
-
-
-init = 0
+init = "0"
 
 app = App("Display Sensor", layout="auto")
-text1 = Text(app, text="ToF sensor: " + str(vl53.range))
-text2 = Text(app, text="Battery Voltage: ")
 
-text1.repeat(500, updateSensor)
+batt = Text(app, text="Battery Voltage: " + init)
+tof = Text(app, text="ToF sensor: " + str(vl53.range))
+ir = Text(app, text="IR sensors: " + init)
+bttn = Text(app, text="Button sensors: " + init)
+mtr = Text(app, text="Motor Speed: " + init)
+
+
+tof.repeat(100, updateTof)
+ir.repeat(50, getIR)
+bttn.repeat(50, getBtns)
+batt.repeat(1000, getVolt)
+mtr.repeat(100, getMotor)
 
 app.display()
 
