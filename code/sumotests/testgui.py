155f8112bf899144eea138ca35b86816298a4aa9
Chetan <dindukuc@oregonstate.edu>
2019-03-12 13:42:42 -0700

    finished gui

--- code/sumotests/testgui.py
+++ code/sumotests/testgui.py
@@ -6,7 +6,7 @@ import board
 import busio
 import adafruit_vl53l0x
 import serial
-
+from serial import *
 from guizero import App, Text
 
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -22,30 +22,35 @@ ser = serial.Serial('/dev/ttyS0',
 
 def getBtns():
     Btns = [0, 0, 0, 0]
-    ser.write(b'H')
-    data = int(ser.read(1).hex(), 16)
+    try:
+        ser.write(b'H')
+        data = ser.read(1)[0]
 
-    Btns[0] =   (data & 0b00001000) >> 3
-    Btns[1] =   (data & 0b00000010) >> 1
-    Btns[2] =   (data & 0b00000001) 
-    Btns[3] =   (data & 0b00000100) >> 2
-    bttn.value = "Button sensors: " + str(Btns[0]) + "/" + str(Btns[1]) + "/" + str(Btns[2]) + "/" + str(Btns[3])
-
+        Btns[0] =   (data & 0b00001000) >> 3
+        Btns[1] =   (data & 0b00000010) >> 1
+        Btns[2] =   (data & 0b00000001) 
+        Btns[3] =   (data & 0b00000100) >> 2
+        bttn.value = "Button sensors: " + str(Btns[0]) + "/" + str(Btns[1]) + "/" + str(Btns[2]) + "/" + str(Btns[3])
+    except:
+        pass
     #return Btns
 
 def getIR():
+    
     IR = [0, 0, 0, 0]
     ser.write(b'I')
-    data = ser.read(1).hex()
+    try:
+        data = ser.read(1)[0]
 
-    IR[0] =   (data & 0b00001000) >> 3
-    IR[1] =   (data & 0b00000010) >> 1
-    IR[2] =   (data & 0b00000001) 
-    IR[3] =   (data & 0b00000100) >> 2
-    ir.value = "IR sensors: " + str(IR[0]) + "/" + str(IR[1]) + "/" + str(IR[2]) + "/" + str(IR[3])
+        IR[0] =   (data & 0b00001000) >> 3
+        IR[1] =   (data & 0b00000010) >> 1
+        IR[2] =   (data & 0b00000001) 
+        IR[3] =   (data & 0b00000100) >> 2
+        ir.value = "IR sensors: " + str(IR[0]) + "/" + str(IR[1]) + "/" + str(IR[2]) + "/" + str(IR[3])
+    except:
+        pass        
 
-
-    #return IR
+        #return IR
 
 def getVolt():
     ser.write(b'E')
@@ -55,11 +60,11 @@ def getVolt():
 def getMotor():
     ser.write(b'M')
     data = ser.read(2).hex()
-	mtr.value = "Motor Speed: " + str(data)
-	#ml = data[0] 
-	#mr = 
-	#speedL = int(data1,16)* (100/127)
-	#speedR = int(data2,16)* (100/127)
+    mtr.value = "Motor Speed: " + str(data)
+    #ml = data[0] 
+    #mr = 
+    #speedL = int(data1,16)* (100/127)
+    #speedR = int(data2,16)* (100/127)
 	
 	   
     
